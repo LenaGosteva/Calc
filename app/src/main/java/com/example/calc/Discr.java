aca2ad8ec9046daf8b0fa3ee68a811cdcf8b71f5
Lyudmila <erfghuji>
2022-11-13 19:05:33 +0300

    +Сalculator_v5f

--- app/src/main/java/com/example/calc/Discr.java
+++ app/src/main/java/com/example/calc/Discr.java
@@ -1,5 +1,7 @@
 package com.example.calc;
 
+import static java.lang.Double.NaN;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -27,38 +29,47 @@ public class Discr extends MainActivity {
         textViewX = findViewById(R.id.x);
 
     }
-    private  double get (EditText editTextA){
-        if (editTextA.getText().toString().isEmpty()){
-            if(!k){
-            Toast.makeText(this, "Поля не могут быть пустыми", Toast.LENGTH_SHORT).show();
-            k = true;}
-            return 0.0;
-        }
-
-        return Double.parseDouble(editTextA.getText().toString());
-    }
+//    private  double get (EditText editTextA){
+//        if (editTextA.getText().toString().isEmpty()){
+//            if(!k){
+//            Toast.makeText(this, "Поля не могут быть пустыми", Toast.LENGTH_SHORT).show();
+//            k = true;}
+//            return NaN;
+//        }
+//
+//        return Double.parseDouble(editTextA.getText().toString());
+//    }
 
     public void click (View view){
-        double a = get(editTextA);
-        double b = get(editTextB);
-        double c = get(editTextC);
-        String x = "";
+        String sa = editTextA.getText().toString();
+        String sb = editTextC.getText().toString();
+        String sc = editTextC.getText().toString();
+        if(!sa.isEmpty() && !sb.isEmpty() && !sc.isEmpty()) {
+            double a = Double.parseDouble(sa);
+            double b = Double.parseDouble(sb);
+            double c = Double.parseDouble(sc);
+            String x = "";
 
-        if (a == 0){
-            x += String.format("%.2f", -c/(double)b);
-            textViewX.setText(x);
+            if (a == 0) {
+                x += String.valueOf(-c /(double)b);
+                textViewX.setText("ОТВЕТ: " + x);
+                Toast.makeText(this, a + "x + " + b + " = " + c, Toast.LENGTH_LONG).show();
+            } else {
+                double d = (double) (b * b - 4 * a * c);
+                if (d < 0) x += "Нет действительных корней";
+                else if (d == 0) x += String.format("%.2f", -b / (double) (2 * a));
+                else if (d > 0) {
+                    double x1 = (double) ((-b + Math.sqrt(d)) / (2 * a));
+                    double x2 = (double) ((-b - Math.sqrt(d)) / (2 * a));
+                    x += String.valueOf(x1) + "\n " + String.valueOf(x2);
+                }
+                textViewX.setText("ОТВЕТ: " + x);
+                Toast.makeText(this, a + "x^2 + " + b + "x + " + c + " = 0", Toast.LENGTH_LONG).show();
+            }
         }
         else{
-            double d = (double) (b * b - 4 * a * c);
-            if (d < 0) x += "Нет действительных корней";
-            else if (d == 0) x += String.format("%.2f", -b / (double) (2 * a));
-            else if (d > 0) {
-                double x1 = (double) ((-b + Math.sqrt(d)) / (2 * a));
-                double x2 = (double) ((-b - Math.sqrt(d)) / (2 * a));
-                x +=  String.format("%.2f", x1) + ", " + String.format("%.2f", x2);
-            }
-            textViewX.setText("ОТВЕТ: "+x);
-            Toast.makeText(this, a + "x^2 + " + b + "x + " + c + " = 0", Toast.LENGTH_LONG).show();
+            Toast.makeText(this, "Поля не могут быть пустыми", Toast.LENGTH_SHORT).show();
+            return;
         }
     }
     public void back(View view) {
--- app/src/main/java/com/example/calc/Linear.java
+++ app/src/main/java/com/example/calc/Linear.java
@@ -1,5 +1,7 @@
 package com.example.calc;
 
+import static java.lang.Double.NaN;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -8,6 +10,8 @@ import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import java.sql.ResultSet;
 
 public class Linear extends MainActivity {
 
@@ -27,22 +31,36 @@ public class Linear extends MainActivity {
         textViewX = findViewById(R.id.x);
 
     }
-    private  double get (EditText editTextA){
-        if (editTextA.getText().toString().isEmpty()){
-            if(!k) {
-                Toast.makeText(this, "Поля не могут быть пустыми /n Иначе по умолчанию подставится ноль", Toast.LENGTH_SHORT).show();
-            k = true;}
-            return 0.0;
-        }
-        return Double.parseDouble(editTextA.getText().toString());
-    }
+//    private  Double get (EditText editTextA){
+//        if (editTextA.getText().toString().isEmpty()){
+//            if(!k) {
+//                Toast.makeText(this, "Поля не могут быть пустыми", Toast.LENGTH_SHORT).show();
+//            k = true;
+//            }
+//            return 0.0;
+//        }
+//        return Double.parseDouble(editTextA.getText().toString());
+//    }
     public void click (View view){
-        double a = get(editTextA);
-        double b = get(editTextB);
-        double c = get(editTextC);
-        double x = (c-b)/(double)a;
-        textViewX.setText(String.format("%.2f", x));
-        Toast.makeText(this, a + "x + " + b + " = " + c, Toast.LENGTH_LONG).show();
+        String sa = editTextA.getText().toString();
+        String sb = editTextC.getText().toString();
+        String sc = editTextC.getText().toString();
+        if(!sa.isEmpty() && !sb.isEmpty() && !sc.isEmpty()) {
+            double a = Double.parseDouble(sa);
+            double b = Double.parseDouble(sb);
+            double c = Double.parseDouble(sc);
+            Toast.makeText(this, a + "x + " + b + " = " + c, Toast.LENGTH_LONG).show();
+            if (a == 0) textViewX.setText("ОТВЕТ: Уравнение не имеет решений" );
+            else {
+                double x = (c-b)/(double)a;
+                textViewX.setText("ОТВЕТ: " +String.valueOf(x));
+            }
+        }
+        else {
+            Toast.makeText(this, "Поля не могут быть пустыми", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
     }
 
     public void back(View view) {
